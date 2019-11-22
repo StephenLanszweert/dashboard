0554b6c7875ad4a3657db84ef52152550425bf7e
Stephen Lanszweert <Stephen.Lanszweert@realdolmen.com>
2019-11-22 21:36:10 +0100

    Updated basic structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Todolist from './Todolist';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <div className='blur'></div>
+            <h1>Good evening Stephen</h1>
+            <div className='content'>
+                <div>
+                    <Todolist></Todolist>
+                </div>
+                <div>
+                    <h1>Test</h1>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Todolist.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Todolist.scss';
+
+export default class Todolist extends Component {
+    render() {
+        return (
+            <div className="todolist">
+                <h2>TodoList</h2>
+            </div>
+        )
+    }
+}
