d4cd815dda8caca3f5c273ea5d337c4e5602ee30
LouisBoucquet <louisboucquet@gmail.com>
2019-11-25 16:13:59 +0100

    implemented part of day

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 export default function Greeting(props) {
 
-	const partOfDay = "evening";
+	const currentDate = new Date();
+	const currentHour = currentDate.getHours();
+
+	let partOfDay = ""
+
+	if (currentHour < 6)
+		partOfDay = "night";
+	else if (currentHour < 12)
+		partOfDay = "morning";
+	else if (currentHour < 18)
+		partOfDay = "afternoon";
+	else if (currentHour < 22)
+		partOfDay = "evening";
+	else
+		partOfDay = "night";
 
 	if (props.personal && props.personal.name)
-		return (
-			<h1>Good {partOfDay} {props.personal.name}</h1>
-		)
-	return (
-		<h1>Good {partOfDay}</h1>
-	)
+		return <h1>Good {partOfDay} {props.personal.name}</h1>
+
+	return <h1>Good {partOfDay}</h1>
 }
