febe34a3deae751777a4ab003d5c97b9ec5a65ef
unknown <louisboucquet@gmail.com>
2020-02-28 10:28:57 +0100

    Made generic Widget class

---
+++ src/components/Widget.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import db, { WidgetDb } from "../db/db";
+
+type Props<T> = {
+	widgetProps: T,
+	id: string
+}
+
+type State<T, Db> = {
+	widgetState?: T,
+	data?: Db
+}
+
+export default class Widget<P, S, Db> extends React.Component<Props<P>, State<S, Db>> {
+
+	widgetDb: Promise<WidgetDb<Db>>;
+
+	constructor(
+		props: Props<P>,
+		private def: Db
+	) {
+		super(props);
+
+		this.widgetDb = db.getWidgetDb(this.props.id, this.def);
+
+		this.state = {};
+	}
+
+	public get id() {
+		return this.props.id
+	}
+
+	async componentDidMount() {
+		this.reloadData();
+	}
+
+	async reloadData() {
+		const widgetDb = await this.widgetDb;
+		const data = await widgetDb.get();
+
+		this.setState({ data });
+	}
+}
